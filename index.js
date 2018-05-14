9b3cf332911e9a462a8cd0fe9c72e2dbcd2568e6
fighterzhou <fighterzhou@126.com>
2018-05-14 09:25:02 +0800

    修改打包入口文件处理类  namedChunkGroups => entrypoints. 避免打包进行代码分割时，无法对模块js进行命名。

--- index.js
+++ index.js
@@ -1,45 +1,101 @@
-'use strict';
+const fs = require('fs');
+const path = require('path');
 
 function dictToHtmlAttribute(obj) {
   return Object.keys(obj).map(k => `${k}="${obj[k]}"`).join(' ');
 }
 
-function HtmlWebpackAssetPlugin() {
+function readTemplate(name) {
+  return fs.readFileSync(`./template/${name}`, { encoding: 'utf8' });
 }
 
-HtmlWebpackAssetPlugin.prototype.apply = function (compiler) {
-  const self = this;
-  compiler.plugin('compilation', function (compilation) {
-    compilation.plugin('html-webpack-plugin-before-html-processing', function (htmlPluginData, callback) {
-      htmlPluginData.html = self.insertAsset(htmlPluginData.html, htmlPluginData.plugin.options);
-      callback(null, htmlPluginData);
+function genScript(i) {
+  if (typeof i === 'string') return `<script type="text/javascript" src="${i}"></script>`;
+  return `<script type="text/javascript" ${dictToHtmlAttribute(i)}></script>`
+}
+
+function genStyle(i) {
+  if (typeof i === 'string') return `<link rel="stylesheet" href="${i}"/>`;
+  return `<link rel="stylesheet" ${dictToHtmlAttribute(i)}/>`
+}
+
+function HTMLPlugin(options) {
+  this.options = options;
+}
+
+HTMLPlugin.prototype.apply = function (compiler) {
+
+  const emit = (compilation, callback) => {
+    const hash = compilation.hash.substr(0, 5);
+    const entrys = new Map();
+
+    compilation.entrypoints.forEach(function (v, k) {
+      const script = [];
+      const css = [];
+
+      v.chunks.forEach(c => {
+        c.files.forEach(f => {
+          if (f.endsWith('.js')) {
+            script.push(f);
+          } else if (f.endsWith('.css')) {
+            css.push(f);
+          }
+        })
+      });
+
+      entrys.set(k, { script, css })
     });
-  });
+
+    const defaultTemplate = readTemplate('index.html');
+
+    const { html, config } = this.options;
+
+    if (config) {
+      const cfg = fs.readFileSync(config, { encoding: 'utf8' });
+      compilation.fileDependencies.add(path.resolve(config));
+      compilation.assets[config] = {
+        source: () => cfg,
+        size: () => cfg.length,
+      };
+    }
+
+    const htmlConfig = {};
+    html.forEach(i => {
+      htmlConfig[i.name] = i;
+    });
+
+    entrys.forEach((v, k) => {
+      let { scripts = [], links = [], metas = [], title = '', template = '' } = htmlConfig[k];
+
+      if (config) {
+        scripts = [`${config}?v=${hash}`, ...scripts];
+      }
+
+      const scriptTags = [...scripts, ...v.script].map(i => genScript(i)).join('\n');
+      const linkTags = [...links, ...v.css].map(i => genStyle(i)).join('\n');
+      const metaTags = metas.map(i => `<meta ${dictToHtmlAttribute(i)} />`).join('\n');
+
+      const templateSource = template ? readTemplate(template) : defaultTemplate;
+      let rs = templateSource.replace(/\n*\s*{{scripts}}/, scriptTags);
+      rs = rs.replace(/\n*\s*{{metas}}/, metaTags);
+      rs = rs.replace(/\n*\s*{{links}}/, linkTags);
+      rs = rs.replace('{{title}}', title);
+
+      const filename = `${k}.html`;
+      compilation.assets[filename] = {
+        source: () => rs,
+        size: () => rs.length,
+      };
+    });
+
+    callback();
+  };
+
+  if (compiler.hooks) {
+    const plugin = { name: 'MyPlugin' };
+    compiler.hooks.emit.tapAsync(plugin, emit);
+  }
+
 };
 
-HtmlWebpackAssetPlugin.prototype.insertAsset = function (html, options) {
-  const { scripts = [], links = [], metas = [], title = '' } = options;
-
-  const scriptTags = scripts.map(i => {
-    if (typeof i === 'string') return `<script type="text/javascript" src="${i}"></script>`;
-    return `<script type="text/javascript" ${dictToHtmlAttribute(i)}></script>`
-  }).join('');
-
-  const linkTags = links.map(i => {
-    if (typeof i === 'string') return `<link rel="stylesheet" href="${i}"/>`;
-    return `<link rel="stylesheet" ${dictToHtmlAttribute(i)}/>`
-  }).join('');
-
-  const metaTags = metas.map(i => {
-    return `<meta ${dictToHtmlAttribute(i)} />`
-  }).join('');
-
-  let rs = html;
-  rs = rs.replace(/\n*\s*{{scripts}}/, scriptTags);
-  rs = rs.replace(/\n*\s*{{metas}}/, metaTags);
-  rs = rs.replace(/\n*\s*{{links}}/, linkTags);
-  rs = rs.replace('{{title}}', title);
-  return rs;
-};
-
-module.exports = HtmlWebpackAssetPlugin;
+module.exports = HTMLPlugin;
